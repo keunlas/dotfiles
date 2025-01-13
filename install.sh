3f24adaa5d276a9fc9c1139b0c84d966b741695e
keunlas <126337231+keunlas@users.noreply.github.com>
2025-01-13 23:16:34 +0800

    up

--- install.sh
+++ install.sh
@@ -1,19 +1,23 @@
-#!/bin/bash
+#!/usr/bin/bash
 
 # Remember. 
 # This repo "dotfiles" 
 # must be put in directory "~".
 
 echo "Creating links to ~/.bashrc"
-ln -sf ~/dotfiles/.bashrc ~/.bashrc
+ln -sf ~/dotfiles/origin/.bashrc ~/.bashrc
 
 echo "Creating links to ~/.vimrc"
-ln -sf ~/dotfiles/.vimrc ~/.vimrc
+ln -sf ~/dotfiles/origin/.vimrc ~/.vimrc
 
 echo "Creating links to ~/.config/kitty/kitty.conf"
-ln -sf ~/dotfiles/.config/kitty/kitty.conf ~/.config/kitty/kitty.conf
+ln -sf ~/dotfiles/origin/.config/kitty/kitty.conf ~/.config/kitty/kitty.conf
 
 echo "Creating links to ~/.config/wofi/config"
-ln -sf ~/dotfiles/.config/wofi/config ~/.config/wofi/config
+ln -sf ~/dotfiles/origin/.config/wofi/config ~/.config/wofi/config
 
 echo "All Done."
+
+
+
+
