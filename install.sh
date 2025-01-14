970ee2003084bac19bfe6953c5a7172296f182b7
keunlas <126337231+keunlas@users.noreply.github.com>
2025-01-14 10:14:29 +0800

    up

--- install.sh
+++ install.sh
@@ -19,11 +19,16 @@ echo "Creating links to ~/.npmrc"
 ln -sf ~/dotfiles/origin/.npmrc ~/.npmrc
 
 echo "Creating links to ~/.config/kitty/kitty.conf"
+mkdir -p ~/.config/kitty
 ln -sf ~/dotfiles/origin/.config/kitty/kitty.conf ~/.config/kitty/kitty.conf
 
 echo "Creating links to ~/.config/wofi/config"
+mkdir -p ~/.config/wofi
 ln -sf ~/dotfiles/origin/.config/wofi/config ~/.config/wofi/config
 
+echo "Creating links to ~/.config/nvim/lua/custom/"
+rm -r ~/.config/nvim/lua/custom
+ln -sf ~/dotfiles/origin/.config/nvim/lua/custom ~/.config/nvim/lua/
 
 echo "===== All Done ====="
 echo ""
---
+++ origin/.config/nvim/lua/custom/basic.lua
@@ -0,0 +1,9 @@
+local opt = vim.opt
+
+opt.tabstop = 2 
+
+opt.softtabstop = 2
+opt.expandtab = true
+
+opt.shiftwidth = 2
+
